e4830342627310714320f6689cb7bcc66e964a32
roger vincent-luce <rogervincentluce@gmail.com>
2021-09-02 09:53:29 +0200

    first commit

---
+++ partial/_navbar.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-3">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Rodger</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="/NewProject/">Accueil
+            
+          </a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link active" href="/NewProject/Test.php">Test</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="/NewProject/LesExercices.php">Exercices</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="/NewProject/PageDeNotes.php">Notes</a>
+        </li>
+        
+      </ul>
+      
+    </div>
+  </div>
+</nav>
