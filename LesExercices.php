d839655ad8ea2eaa44c0aa1472dad4cf4170de4a
roger vincent-luce <rogervincentluce@gmail.com>
2021-09-03 16:17:07 +0200

    premier exo

--- LesExercices.php
+++ LesExercices.php
@@ -11,6 +11,10 @@
 <?php include("./partial/_navbar.php");?>
 <div class="container">
     <h1>Exercices php</h1>
+    <h2>Exercice 3</h2>
+    <ul>
+        <li><a href="/NewProject/exo3.php"> exercice3 </a>les tableaux</li>
+    </ul>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
